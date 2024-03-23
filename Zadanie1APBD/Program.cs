71e118076c9909d637bac57a5b2eec35fd5ce14d
s20350 <s20350@pjwstk.edu.pl>
2024-03-23 10:07:04 +0100

    zmiana1

--- Zadanie1APBD/Program.cs
+++ Zadanie1APBD/Program.cs
@@ -7,8 +7,8 @@ namespace Zadanie1APBD
         public static void Main(string[] args)
         {
             //zmiany
-            int[] numbers = { 1, 2, 3, 4, 5 };
-            double average = CalculateAverage(numbers);
+            int[] numbers1 = { 1, 2, 3, 4, 5 };
+            double average = CalculateAverage(numbers1);
             Console.WriteLine($"Średnia: {average}");
         }
 
